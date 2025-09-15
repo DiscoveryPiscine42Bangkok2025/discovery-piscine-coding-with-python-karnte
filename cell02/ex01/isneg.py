07a7bf7ee069ca63d9d15cb2585454913f57d2c1
karnte <karneieizaza@gmail.com>
2025-09-16 00:00:22 +0700

    submit(ex01) : upload assignment folder with script

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+num = int(input())
+if num < 0:
+    print("This number is negetive.")
+elif num > 0:
+    print("This number is positive.")
+else:
+    print("This number is both positive and negative.")
