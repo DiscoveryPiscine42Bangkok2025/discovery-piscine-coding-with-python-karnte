e2453924ce9c2e7dc5bb68241428502238dc91f7
karnte <karneieizaza@gmail.com>
2025-09-15 22:57:35 +0700

    submit(ex02): upload assignment folder with whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+firstName = input("Hey, what's your first name? :").strip()
+lastName = input("And your last name? : ").strip()
+print("Well, pleased to meet you, " + firstName + " " + lastName + ".")
