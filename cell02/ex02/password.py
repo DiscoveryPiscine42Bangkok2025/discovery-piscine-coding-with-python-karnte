737714818a387487e21e1456b393bb1b68207ecb
karnte <karneieizaza@gmail.com>
2025-09-16 14:20:42 +0700

    submit(ex02) : upload assignment folder with script

---
+++ cell02/ex02/password.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+user_input = input("Enter Password : ").strip()
+password = "Python is awesome"
+if user_input == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
