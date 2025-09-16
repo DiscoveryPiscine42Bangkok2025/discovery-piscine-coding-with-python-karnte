bf862705ee0d504c945e62d0cb4ecf896e0584f5
karnte <karneieizaza@gmail.com>
2025-09-16 17:15:40 +0700

    submit(ex01) : upload assignment folder with script

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+multiplier = int(input("Enter a number \n"))
+counter = 0
+while counter < 10:
+    print(f"{counter} X {multiplier} = {counter * multiplier}")
+    counter += 1
