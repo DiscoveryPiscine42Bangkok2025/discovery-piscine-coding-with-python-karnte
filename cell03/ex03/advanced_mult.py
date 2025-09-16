c4eb5d3a0a0d89897b704293bc2145c1b4e57317
karnte <karneieizaza@gmail.com>
2025-09-16 17:57:32 +0700

    submit(ex03) : upload folder with script

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+i = 0
+while i <= 10:
+    print(f"Table de {i}:", end=" ")
+    j = 0
+    while j <= 10:
+        result = i * j
+        print(result, end=" " if j < 10 else "")
+        j += 1
+    print()
+    i += 1
